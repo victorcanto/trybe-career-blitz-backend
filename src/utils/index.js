29a80b7dc0c4016257e1a3b2905ae69e32e7d3d0
Victor Canto <iamvictorcanto@gmail.com>
2021-11-02 15:54:10 -0300

    feat: utils

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+const jwt = require('./jwt');
+const ValidateError = require('./ValidateError');
+const ErrorMessages = require('./ErrorMessages');
+
+module.exports = {
+  jwt,
+  ErrorMessages,
+  ValidateError,
+};

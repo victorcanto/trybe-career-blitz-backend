bb08a5c3095f95102f1d146419224b3716f04f7c
Victor Canto <iamvictorcanto@gmail.com>
2021-11-02 17:14:36 -0300

    feat: controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,9 @@
+const UserController = require('./userController');
+const TaskController = require('./taskController');
+const LoginController = require('./loginController');
+
+module.exports = {
+  UserController,
+  TaskController,
+  LoginController,
+};

27fb849b2d8cd922876933baa57a734e94ffc836
Victor Canto <iamvictorcanto@gmail.com>
2021-11-02 17:15:22 -0300

    feat: app and server

--- src/api/app.js
+++ src/api/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (_req, res) => {
-  res.status(200).json({ message: 'Servidor ok' });
-});
+const { userRouter, taskRouter, loginRouter } = require('../routes');
+const { ErrorMiddleware } = require('../middlewares');
+
+app.use('/user', userRouter);
+app.use('/task', taskRouter);
+app.use('/login', loginRouter);
+
+app.use(ErrorMiddleware);
 
 module.exports = { app };
